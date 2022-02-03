41235dca6c91f718f2d92c2c5e5a56d5fc3c4d98
fvn70 <fvn_reg@mail.ru>
2022-02-03 22:56:54 +0700

    Bill Splitter. Stage 2/4: The bill has arrived

--- Bill Splitter/task/billsplitter.py
+++ Bill Splitter/task/billsplitter.py
@@ -1,3 +1,4 @@
+
 num = int(input("Enter the number of friends joining (including you):\n"))
 if num <= 0:
     print("\nNo one is joining for the party")
@@ -7,4 +8,12 @@ else:
     for n in range(num):
         name = input()
         dic[name] = 0
+
+    bill = int(input("Enter the total bill value:\n"))
+    check = bill / num
+    check = round(check, 2) if check != bill // num else int(check)
+
+    for d in dic:
+        dic[d] = check
+
     print(dic)
